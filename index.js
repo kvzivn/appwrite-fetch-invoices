daa9049d7bc11ff8dd62809d2a089336385df363
Kevin <hello@kevinivan.com>
2024-06-04 13:31:26 +0200

    add function

---
+++ index.js
@@ -0,0 +1,16 @@
+export default async ({ res, error }) => {
+  try {
+    const response = await fetch("https://api.eukapay.com/invoices", {
+      headers: {
+        "x-api-key": process.env.EUKAPAY_API_KEY,
+        "Content-Type": "application/json",
+      },
+    })
+
+    const data = await response.json()
+    return res.json(data)
+  } catch (err) {
+    error("Error fetching data from EUKAPAY API:", err.message)
+    return res.empty()
+  }
+}
