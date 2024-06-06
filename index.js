2e71b26e1b483becde0daaeb1df9109ab06599a5
Kevin <hello@kevinivan.com>
2024-06-06 14:43:05 +0200

    add pagination

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-export default async ({ res, error }) => {
-  try {
-    const response = await fetch("https://api.eukapay.com/invoices?limit=100", {
-      headers: {
-        "x-api-key": process.env.EUKAPAY_API_KEY,
-        "Content-Type": "application/json",
-      },
-    })
+export default async ({ res, log, error }) => {
+  async function fetchInvoices(pageParam = {}) {
+    let invoices = []
+    let hasMore = true
 
-    const data = await response.json()
-    return res.json(data)
+    while (hasMore) {
+      let query = `https://api.eukapay.com/invoices?limit=20`
+
+      if (pageParam.startingAfter) {
+        query += `&starting_after=${pageParam.startingAfter}`
+      }
+
+      const response = await fetch(query, {
+        headers: {
+          "x-api-key": process.env.EUKAPAY_API_KEY,
+          "Content-Type": "application/json",
+        },
+      })
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok")
+      }
+
+      const data = await response.json()
+      invoices = invoices.concat(data.data)
+      hasMore = data.has_more
+
+      if (hasMore) {
+        pageParam.startingAfter = data.data[data.data.length - 1].code
+      }
+    }
+
+    log("Fetched invoices:", invoices.length)
+    return invoices
+  }
+
+  try {
+    const allInvoices = await fetchInvoices()
+    return res.json(allInvoices)
   } catch (err) {
     error("Error fetching data from EUKAPAY API:", err.message)
-    return res.empty()
+    return res.status(500).json({ error: "Internal Server Error" })
   }
 }
